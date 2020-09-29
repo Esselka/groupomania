845c33220fe8056aaf68203c3967ae0da51a73ae
Alexis Leclercq <slakenote@gmail.com>
2020-09-29 03:29:19 +0200

    update fichier DB

--- Groupomania_DB.sql
+++ Groupomania_DB.sql
@@ -1,16 +1,77 @@
-CREATE DATABASE  IF NOT EXISTS `groupomania` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `groupomania`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: groupomania
 -- ------------------------------------------------------
 -- Server version	8.0.21
 
-DROP DATABASE IF EXISTS groupomania;
-CREATE DATABASE groupomania CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_bin;
-CREATE USER 'admin-groupo' IDENTIFIED BY 'Projet7_OC';
-GRANT ALL ON groupomania.* TO 'admin-groupo';
+DROP DATABASE IF EXISTS `groupomania`;
+CREATE DATABASE `groupomania` CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE USER 'user' IDENTIFIED BY 'Projet7_OC';
+GRANT ALL ON groupomania.* TO 'user';
 USE groupomania;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(50) COLLATE utf8_bin NOT NULL,
+  `username` varchar(50) COLLATE utf8_bin NOT NULL,
+  `email` varchar(250) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL,
+  `avatar_url` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT 'http://localhost:3000/images/defaultPic.png',
+  `register_date` datetime NOT NULL,
+  `role` varchar(45) COLLATE utf8_bin DEFAULT 'contributor',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (22,'admin','admin','admin','admin@groupomania.com','$2b$10$DHH7AFsyP598xkl2uItLCu2uc4D8VEqsjRmBqeqfJ2LLwGTUd04Iu','http://localhost:3000/images/defaultPic.png','2020-09-28 20:37:14','admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `date` datetime NOT NULL,
+  `title` varchar(50) COLLATE utf8_bin NOT NULL,
+  `image_url` varchar(200) COLLATE utf8_bin NOT NULL,
+  `slug` varchar(25) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`post_id`),
+  UNIQUE KEY `slug_UNIQUE` (`slug`),
+  KEY `fk_post_userID_idx` (`user_id`),
+  CONSTRAINT `fk_post_userID` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `comments`
@@ -35,47 +96,13 @@ CREATE TABLE `comments` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `posts`
+-- Dumping data for table `comments`
 --
 
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `posts` (
-  `post_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int unsigned NOT NULL,
-  `date` datetime NOT NULL,
-  `title` varchar(50) COLLATE utf8_bin NOT NULL,
-  `image_url` varchar(200) COLLATE utf8_bin NOT NULL,
-  `slug` varchar(25) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`post_id`),
-  UNIQUE KEY `slug_UNIQUE` (`slug`),
-  KEY `fk_post_userID_idx` (`user_id`),
-  CONSTRAINT `fk_post_userID` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) COLLATE utf8_bin NOT NULL,
-  `lastname` varchar(50) COLLATE utf8_bin NOT NULL,
-  `username` varchar(50) COLLATE utf8_bin NOT NULL,
-  `email` varchar(250) COLLATE utf8_bin NOT NULL,
-  `password` varchar(100) COLLATE utf8_bin NOT NULL,
-  `avatar_url` varchar(250) COLLATE utf8_bin NOT NULL DEFAULT 'http://localhost:3000/images/defaultPic.png',
-  `register_date` datetime NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `votes`
@@ -96,6 +123,15 @@ CREATE TABLE `votes` (
   CONSTRAINT `fk_vote_userID` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `votes`
+--
+
+LOCK TABLES `votes` WRITE;
+/*!40000 ALTER TABLE `votes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `votes` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -106,4 +142,4 @@ CREATE TABLE `votes` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-26 18:04:01
+-- Dump completed on 2020-09-29  3:16:34
